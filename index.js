706134f8c3a6c6afe1ce2675f0954c768ebd068a
Prasanna Nimbalkar <nimbalkar.p@northeastern.edu>
2022-11-26 09:59:06 -0500

    Updating assignment9

--- index.js
+++ index.js
@@ -1,76 +1,50 @@
-var aws = require("aws-sdk");
+const aws = require("aws-sdk");
 aws.config.update({region:'us-east-1'})
-var ses = new aws.SES({ region: "us-east-1" });
-var docClient = new aws.DynamoDB.DocumentClient();
-
+const ses = new aws.SES({ region: "us-east-1" });
+const dynamoDatabase = new aws.DynamoDB({
+    apiVersion: '2012-08-10',
+    region: process.env.AWS_REGION || 'us-east-1'
+});
 exports.handler = async function (event) {
-    let message = event.Records[0].Sns.Message
-    let json = JSON.parse(message);
-    let email = json.username;
-    let token = json.token;
-    const seconds = 5 * 60;
+    const message = event.Records[0].Sns.Message
+    const messageFromWebApp = JSON.parse(message);
+    const emailAddress = messageFromWebApp.username;
+    const token = messageFromWebApp.token;
+    const seconds = 300;
     const secondsInEpoch = Math.round(Date.now() / 1000);
     const expirationTime = secondsInEpoch + seconds;
-    const currentTime = Math.round(Date.now() / 1000);
-
-    //Creating a table for DynamoDB
-
-    var table = {
-        TableName : "csye-6225",
-        Item:{
-        "username" : email,
-        "token" : token,
-        "TimeToExist" :expirationTime
-        }
-    }
-    console.log("Adding new item");
-
-  //Putting an item to DynamoDB Table
-    docClient.put(table, function(err, data) {
-        if (err) {
-            console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-        } else {
-            console.log("Added:", JSON.stringify(data, null, 2));
-        }
-    });
-    console.log(email + " " +token + "  Parameters set!!");
-
-    const mailbody = `
-    <!DOCTYPE html>
-    <html>
-        <head>
-        </head>
-        <body>
-        <p>Hi, ${email}</p>
-        <p>Please verify your email</br>
-        <b>Link will be valid only for 5 minutes!!</b></br>
-        Find your link below:</p>
-        <p><a href=http://prod.prasannanimbalkar.me/v1/user/verifyUserEmail?token=${token}&email=${email} >
-            http://demo.prasannanimbalkar.me/v1/user/verifyUserEmail?token=${token}&email=${email} </a> </p>
-            </body></html>
-        </body>
-    </html>`;
-
-    var params = {
-    
-    Destination: {
-        ToAddresses: [email],
+const body = `
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+      <p>Hi, ${emailAddress}</p>
+      <p>Please verify your email</br>
+      <b>Link will be valid only for 5 minutes!</b></br>
+      Find your link below:</p>
+      <p><a href=prod.prasannanimbalkar.me:3000/v1/verifyUserEmail?token=${token}&email=${emailAddress} >
+        prod.prasannanimbalkar.me:3000/v1/verifyUserEmail?token=${token}&email=${emailAddress} </a> </p>
+        </body></html>
+    </body>
+</html>`;
+const params = {
+  Destination: {
+    ToAddresses: [emailAddress],
+  },
+  Message: {
+    Body: {
+      Html: {
+        Charset: "UTF-8",
+        Data: body,
+      },
+    }, 
+    Subject: {
+        Charset: "UTF-8",
+        Data: "Please verify your email for Prazz's website",
+      },
     },
-    Message: {
-        Body: {
-        Html: {
-            Charset: "UTF-8",
-            Data: mailbody,
-        },
-        }, 
-        Subject: {
-            Charset: "UTF-8",
-            Data: "Email Verification",
-        },
-        },
-        Source: "sender@prod.prasannanimbalkar.me",
-    };
-    console.log("email sent");
-    return ses.sendEmail(params).promise()
-  
-};           
+    Source: "reply@prod.prasannanimbalkar.me",
+  };
+  return ses.sendEmail(params).promise()
+};
